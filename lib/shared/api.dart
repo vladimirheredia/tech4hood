e14005a9d10b82033fef385d3a103cc13b93e6a9
Vladimir Heredia <vheredia04@hotmail.com>
2019-02-26 23:23:36 -0500

    Updated api file to make other calls for videos, resources, instagram etc.

--- lib/shared/api.dart
+++ lib/shared/api.dart
@@ -1,25 +1,42 @@
+import 'dart:async';
 import 'package:http/http.dart' as http;
 
 const baseUrl = 'https://us-central1-tech4hood.cloudfunctions.net';
 
-class API{
-
+class API {
   //gets all events from meetup api
-  static Future getAllEvents() async{
+  static Future getAllEvents() async {
     var url = baseUrl + '/getEvents';
     return await http.get(url);
   }
 
   //gets all the members from meetup api
-  static Future getAllMembers() async{
+  static Future getAllMembers() async {
     var url = baseUrl + '/getMembers';
     return await http.get(url);
   }
 
   //gets all the tweets from Twitter api
-  static Future getAllTweets() async{
+  static Future getAllTweets() async {
     var url = baseUrl + '/getTweets';
     return await http.get(url);
   }
-  
+
+  //gets all instagram feeds
+  static Future getInstragramFeed() async {
+    var url = baseUrl + '/getInstagramFeed';
+    return await http.get(url);
+  }
+
+  //gets all resources
+  static Future getResources() async {
+    var url = baseUrl + '/getResources';
+    return await http.get(url);
+  }
+
+  //get youtube videos
+  static Future getYoutubeVideos() async {
+    var url = baseUrl + '/getResources';
+    return await http.get(url);
+  }
 }
